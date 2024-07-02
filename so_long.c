1ce753f349f66d3564788979f51cd12fcea98410
Iriana R <dev32.irin@gmail.com>
2024-07-02 06:37:28 +0300

    solong2

--- so_long.c
+++ so_long.c
@@ -14,6 +14,18 @@
 #include <X11/keysym.h>
 #include <stdio.h>
 #define BUFFER_SIZE 1024
+typedef struct s_coord 
+{
+	int x;
+	int y;
+}	s_coord;
+typedef struct s_data
+{
+	void *mlx;
+	void *win;
+	char **map;
+	s_coord pos;
+}	t_data;
 
 void mtoa(char ***arr, int lines, int col, char *buf)
 {
@@ -95,63 +107,85 @@ int ft_len(char **s)
 	return (i);
 }
 
+
+
+void *img_return(char *path, t_data *mlx)
+{
+	int width;
+	int height;
+	return (mlx_xpm_file_to_image(mlx->mlx, path, &width, &height));
+}
+
+void put_pos(char **matrix_map, t_data mlx, s_coord pos, int x, int y)
+{
+	mlx_clear_window(mlx.mlx, mlx.win);
+	while (matrix_map[pos.y] != NULL)
+	{
+		pos.x = 0;
+		while (matrix_map[pos.y][pos.x] != '\0')
+		{ 
+			if (matrix_map[pos.y][pos.x] == 'P')
+                                mlx_put_image_to_window(mlx.mlx, mlx.win, img_return("./assets/Terrain/characters.xpm", &mlx), (pos.x) * 48 + x, pos.y * 48 + y);
+                        
+			if (matrix_map[pos.y][pos.x] == '1')
+                                mlx_put_image_to_window(mlx.mlx, mlx.win, img_return("./assets/Terrain/wall.xpm", &mlx), pos.x * 48, pos.y * 48);
+                        if (matrix_map[pos.y][pos.x] == 'C')
+                                mlx_put_image_to_window(mlx.mlx, mlx.win, img_return("./assets/Terrain/strawberry.xpm", &mlx), pos.x * 48, pos.x * 48);
+                       if (matrix_map[pos.y][pos.x] == 'E')
+                                mlx_put_image_to_window(mlx.mlx, mlx.win, img_return("./assets/Terrain/exit.xpm", &mlx), pos.x * 48, pos.y * 48);
+
+                        pos.x++;
+		}
+		pos.y++;
+	}
+}
+
+
 int key_press(int keycode, void *param)
 {
-	if(keycode == 53)
+	t_data *data = (t_data *)param;
+	static int  x = 0;
+	static int y = 0;
+	int new_x;
+	int new_y;
+
+	new_x = x;
+	new_y = y;
+	if(keycode == XK_Escape)
 	{
-		printf("Escape pressed");
-		mlx_destroy_window(param, param);
+		mlx_destroy_window(data->mlx, data->win);
 		exit(0);
 	}
+	else if(keycode == XK_d)
+		new_x += 48;
+	else if(keycode == XK_a)
+		new_x -= 48;
+	else if(keycode == XK_w)
+		new_y -= 48;
+	else if(keycode == XK_s)
+		new_y += 48;
+	
+	printf("%c\n", data->map[new_x/48][new_y/48]);
+	x = new_x;
+	y = new_y;
+	put_pos(data->map, *data, data->pos, x, y);
+
 	return (0);
 }
 
 int main()
 {
-	char **arr;
 	int fd;
-	void *mlx;
-	void *mlx_win;
-	void *img_wall;
-	void *img_collectible;
-	void *img_characters;
-	void *img_exit;
-	int img_width;
-	int img_height;
-	int i;
-	int k;
+	t_data mlx;
 
-	mlx = mlx_init();
-	img_wall = mlx_xpm_file_to_image(mlx, "./assets/Terrain/wall.xpm", &img_width, &img_height);
-	img_collectible = mlx_xpm_file_to_image(mlx, "./assets/Terrain/strawberry.xpm", &img_width, &img_height);
-	img_characters = mlx_xpm_file_to_image(mlx, "./assets/Terrain/characters.xpm", &img_width, &img_height);
-	img_exit = mlx_xpm_file_to_image(mlx, "./assets/Terrain/exit.xpm", &img_width, &img_height);
-
-	i = 0;
+	mlx.pos.x = 0;
+	mlx.pos.y = 0;
+	mlx.mlx = mlx_init();
 	fd = open("carte2.ber", O_RDONLY);
-	arr = count_lines_map(fd);
-	mlx_win = mlx_new_window(mlx, 48 * ft_strlen(arr[0]), 48 * ft_len(arr), "solong");
-	k = 0;
-	while (arr[i] != NULL)
-	{
-		k = 0;
-		while(arr[i][k] != '\0')
-		{
-			if (arr[i][k] == '1')
-				mlx_put_image_to_window(mlx, mlx_win, img_wall, k * 48, i * 48);
-			if (arr[i][k] == 'C')
-				mlx_put_image_to_window(mlx, mlx_win, img_collectible, k * 48, i * 48);
-			if (arr[i][k] == 'P')
-				mlx_put_image_to_window(mlx, mlx_win, img_characters, k * 48, i * 48);
-			if (arr[i][k] == 'E')
-				mlx_put_image_to_window(mlx, mlx_win, img_exit, k * 48, i * 48);
-				
-
-			k++;
-		}
-		i++;
-	}
-	mlx_key_hook(mlx_win, key_press, mlx);
-	mlx_loop(mlx);
-	free_double(arr, ft_len(arr));
+	mlx.map = count_lines_map(fd);
+	mlx.win = mlx_new_window(mlx.mlx, 48 * ft_strlen(mlx.map[0]), 48 * ft_len(mlx.map), "solong");
+	put_pos(mlx.map,  mlx, mlx.pos, 0, 0);
+	mlx_key_hook(mlx.win, key_press, &mlx);
+	mlx_loop(mlx.mlx);
+	free_double(mlx.map, ft_len(mlx.map));
 }
