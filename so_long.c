7b08403b8873c3df2697239680ac2bcf9f58919c
Iriana R <dev32.irin@gmail.com>
2024-07-08 17:04:15 +0300

    fix collision

--- so_long.c
+++ so_long.c
@@ -24,7 +24,7 @@ typedef struct s_data
 	void *mlx;
 	void *win;
 	char **map;
-	s_coord pos_pers;
+	s_coord *pos_pers;
 	s_coord pos;
 }	t_data;
 
@@ -52,6 +52,37 @@ void mtoa(char ***arr, int lines, int col, char *buf)
 	}
 	(*arr)[lines] = NULL;
 }
+
+s_coord	*catch_pos(char **map)
+{
+	int i;
+	int j;
+	s_coord	*coord;
+
+	i = 0;
+	coord = malloc(sizeof(s_coord));
+	if (NULL == coord)
+		return (NULL);
+	coord->x = 0;
+	coord->y = 0;
+	while (map[i] != NULL)
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == 'P')
+			{
+				coord->x =j;
+				coord->y = i;
+				return (coord);
+			}
+			++j;
+		}
+		++i;
+	}
+	return (coord);
+}
+
 
 char **count_lines_map(int fd)
 {
@@ -140,6 +171,7 @@ int valid_move_bottom(char **map, int x, int y)
 void put_pos(char **matrix_map, t_data mlx, s_coord pos, int x, int y)
 {
 	mlx_clear_window(mlx.mlx, mlx.win);
+	
 	while (matrix_map[pos.y] != NULL)
 	{
 		pos.x = 0;
@@ -147,16 +179,21 @@ void put_pos(char **matrix_map, t_data mlx, s_coord pos, int x, int y)
 		{ 
 			if (matrix_map[pos.y][pos.x] == 'P')
 				mlx_put_image_to_window(mlx.mlx, mlx.win, img_return("./assets/Terrain/characters.xpm", &mlx), pos.x * 48 + x, pos.y * 48 + y);
-            if (matrix_map[pos.y][pos.x] == '1')
-            	mlx_put_image_to_window(mlx.mlx, mlx.win, img_return("./assets/Terrain/wall.xpm", &mlx), pos.x * 48, pos.y * 48);
-            if (matrix_map[pos.y][pos.x] == 'C')
-            	mlx_put_image_to_window(mlx.mlx, mlx.win, img_return("./assets/Terrain/strawberry.xpm", &mlx), pos.x * 48, pos.x * 48);
-            if (matrix_map[pos.y][pos.x] == 'E')
-                mlx_put_image_to_window(mlx.mlx, mlx.win, img_return("./assets/Terrain/exit.xpm", &mlx), pos.x * 48, pos.y * 48);
-            pos.x++;
+	            	if (matrix_map[pos.y][pos.x] == '1')
+        		    	mlx_put_image_to_window(mlx.mlx, mlx.win, img_return("./assets/Terrain/wall.xpm", &mlx), pos.x * 48, pos.y * 48);
+            		if (matrix_map[pos.y][pos.x] == 'C')
+            			mlx_put_image_to_window(mlx.mlx, mlx.win, img_return("./assets/Terrain/strawberry.xpm", &mlx), pos.x * 48, pos.x * 48);
+            		if (matrix_map[pos.y][pos.x] == 'E')
+                		mlx_put_image_to_window(mlx.mlx, mlx.win, img_return("./assets/Terrain/exit.xpm", &mlx), pos.x * 48, pos.y * 48);
+            		pos.x++;
 		}
 		pos.y++;
 	}
+}
+
+int invalid_move(int x, int y, char **map)
+{
+	return (x < 0 || y < 0 || x > ft_strlen(map[0]) || y > ft_len(map) - 1);
 }
 
 
@@ -166,22 +203,44 @@ int key_press(int keycode, void *param)
 	static int  x = 0;
 	static int y = 0;
 	
+		
 	if(keycode == XK_Escape)
 	{
 		mlx_destroy_window(data->mlx, data->win);
 		exit(0);
 	}
 	else if(keycode == XK_d)
+	{
+		if (invalid_move(data->pos_pers->x + 1, data->pos_pers->y, data->map) || (data->map[data->pos_pers->y][data->pos_pers->x + 1] == '1'))
+			return (0);
+		(data->pos_pers->x)++;
 		x += 48;
+	}
 	else if(keycode == XK_a)
+	{
+	
+		if (invalid_move(data->pos_pers->x - 1, data->pos_pers->y, data->map) ||  (data->map[data->pos_pers->y][data->pos_pers->x - 1] == '1') )
+			return (0);
+		(data->pos_pers->x)--;
 		x -= 48;
+	}
 	else if(keycode == XK_w)
+	{
+		if (invalid_move(data->pos_pers->x, data->pos_pers->y - 1, data->map) || (data->map[data->pos_pers->y - 1][data->pos_pers->x] == '1'))
+			return (0);
+		(data->pos_pers->y)--;
 		y -= 48;
+	}
 	else if(keycode == XK_s)
+	{
+		if (invalid_move(data->pos_pers->x, data->pos_pers->y + 1, data->map) || (data->map[data->pos_pers->y + 1][data->pos_pers->x] == '1'))
+			return (0);
+		(data->pos_pers->y)++;
 		y += 48;
-	
+	}
+	//if(data->pos_pers->x < 0 || data->pos_pers->y < 0 || data->pos_pers->x > ft_strlen(data->map[0]) 
+	//		|| data->pos_pers->y > ft_len(data->map))
 	put_pos(data->map, *data, data->pos, x, y);
-	
 	return (0);
 }
 
@@ -197,6 +256,7 @@ int main()
 	mlx.map = count_lines_map(fd);
 	mlx.win = mlx_new_window(mlx.mlx, 48 * ft_strlen(mlx.map[0]), 48 * ft_len(mlx.map), "solong");
 	put_pos(mlx.map,  mlx, mlx.pos, 0, 0);
+	mlx.pos_pers = catch_pos(mlx.map);
 	mlx_key_hook(mlx.win, key_press, &mlx);
 	mlx_loop(mlx.mlx);
 	free_double(mlx.map, ft_len(mlx.map));
